3e567479c7b9020e45eed7dcab46d9bc5a4a84d0
Anas Ghnaim <ghnaimanas20@gmail.com>
2026-01-11 21:51:24 +0300

    remove dummy array for comments

--- src/sockets/comment.ts
+++ src/sockets/comment.ts
@@ -1,23 +1,17 @@
 import { Server, Socket } from "socket.io";
 
 interface Comment {
-  commentId: string;
+  id: string;
   userName: string;
-  time: string;
   description: string;
+  time: string;
   postId: string;
 }
-
-const comments: Comment[] = [];
 
 export function commentsSocket(io: Server, socket: Socket) {
   socket.on("join-post", (postId: string) => {
     socket.join(`post:${postId}`);
     console.log(`Socket ${socket.id} joined post:${postId}`);
-
-    // Send all existing comments for this post
-    const postComments = comments.filter((c) => c.postId === postId);
-    socket.emit("comments:init", postComments);
   });
 
   socket.on("leave-post", (postId: string) => {
@@ -26,7 +20,6 @@ export function commentsSocket(io: Server, socket: Socket) {
   });
 
   socket.on("create-comment", (comment: Comment) => {
-    comments.push(comment);
     io.to(`post:${comment.postId}`).emit("new-comment", comment);
   });
 }
